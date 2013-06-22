38056df59222a9ae722d05a770ea9e02ac41ec24
Alexander Ferguson <x@echonotation.com>
2013-06-22 06:54:52 -0400

    More commenting to FileView.py, along with a few proposed changes commented in.

--- app/FileView.py
+++ app/FileView.py
@@ -8,16 +8,16 @@ from hurry.filesize import size, alternative
 
 info = []
 # Sets the base file directories for the fileviewer
-root = os.path.abspath('tree')
-dir_top = os.path.abspath('tree')
+dir_root = os.path.abspath('tree')
 
 
-def folder_size(dirName):
+# Determines the file size of a folder by summing the file sizes of its children
+def folder_size(dir_path):
     # Resets total_size for recursive / scope purposes
     total_size = 0
     # Runs through the child files and folders to create a sum of file sizes
-    for item in os.listdir(dirName):
-        item_path = os.path.join(dirName, item)
+    for item in os.listdir(dir_path):
+        item_path = os.path.join(dir_path, item)
         if os.path.isfile(item_path):
             total_size += os.path.getsize(item_path)
         elif os.path.isdir(item_path):
@@ -26,17 +26,18 @@ def folder_size(dirName):
     return total_size
 
 
-def walk(root):  # Is this the best variable name ?
+# Walks a file directory and appends the file information to the global variable info
+def walk(dir_path):
     dirs_holder = []
     files_holder = []
 
     # Loops through all files and folders in the directory
-    for i in os.listdir(root):
+    for i in os.listdir(dir_path):
         # Assigns all folders to dirs_holder
-        if os.path.isdir(os.path.join(root, i)):
+        if os.path.isdir(os.path.join(dir_path, i)):
             dirs_holder.append(i)
         # Assigns all files to files_holder
-        elif os.path.isfile(os.path.join(root, i)):
+        elif os.path.isfile(os.path.join(dir_path, i)):
             files_holder.append(i)
         else:  # Continues when the loop is complete - Is this necessary? Wouldn't it continue anyway?
             continue  # Are there other possibilities (not file or dir) that may cause this to break the loop early?
@@ -45,8 +46,8 @@ def walk(root):  # Is this the best variable name ?
     for d in dirs_holder:
         appender = {}
         appender['type'] = 'folder'
-        # Sets the base path top to dir_top
-        appender['path'] = os.path.join(root, d).split(dir_top + os.sep)[1]
+        # Sets the base path top to dir_root
+        appender['path'] = os.path.join(dir_path, d).split(dir_root + os.sep)[1]
         parent = appender['path'].split(os.sep)
         # If it has a parent, add the parent information
         if len(parent) > 1:
@@ -56,17 +57,28 @@ def walk(root):  # Is this the best variable name ?
         # If it doesn't have a parent, just return the path information
         else:
             appender['name'] = appender['path']
+        ## Below is my proposed change -- since the os commands exist, I believe it improves readability ##
+        # parent = os.path.dirname(appender['path'])
+        # # If it has a parent, add the parent information.
+        # if parent:
+        #     appender['name'] = os.path.basename(appender['path'])
+        #     appender['parent_path'] = parent
+        #     appender['parent'] = os.path.basename(parent)
+        # # If it doesn't have a parent, just return the path information.
+        # else:
+        #     appender['name'] = appender['path']
+        ## End proposed change ##
         # Calls the function folder_size to get the folder size
-        appender['size'] = folder_size(os.path.join(root, d))
+        appender['size'] = folder_size(os.path.join(dir_path, d))
         info.append(appender)
         # Recursively calls the walk function on all child files and folders
-        walk(os.path.join(root, d))
+        walk(os.path.join(dir_path, d))
 
     # Loops through the files in the file system
     for f in files_holder:
         appender = {}
         appender['type'] = 'file'
-        appender['path'] = os.path.join(root, f).split(dir_top + os.sep)[1]
+        appender['path'] = os.path.join(dir_path, f).split(dir_root + os.sep)[1]
         parent = appender['path'].split(os.sep)
         # If it has a parent, add the parent information.
         if len(parent) > 1:
@@ -76,71 +88,83 @@ def walk(root):  # Is this the best variable name ?
         # If it doesn't have a parent, just return the path information.
         else:
             appender['name'] = appender['path']
-        appender['size'] = os.path.getsize(os.path.join(root, f))
+        ## Below is my proposed change -- since the os commands exist, I believe it improves readability ##
+        # parent = os.path.dirname(appender['path'])
+        # # If it has a parent, add the parent information.
+        # if parent:
+        #     appender['name'] = os.path.basename(appender['path'])
+        #     appender['parent_path'] = parent
+        #     appender['parent'] = os.path.basename(parent)
+        # # If it doesn't have a parent, just return the path information.
+        # else:
+        #     appender['name'] = appender['path']
+        ## End proposed change ##
+        appender['size'] = os.path.getsize(os.path.join(dir_path, f))
         info.append(appender)
 
 
+# Generates an HTML table for Treetable.js - Returns html #
 def table_gen(info):
     info = info
-    fs = {'paths': {}}  # Clearer, more semantic name?
+    dir_parts = {'paths': {}}
     html = '<thead>\n<tr>\n<th>Name</th>\n<th>Kind</th>\n<th>Size</th>\n</tr>\n</thead>\n<tbody>'
 
     # Loops through info to create a hierarchy for Treetable.js
     for info_dict in info:
-        relative_root = info_dict['path']  # Can this be combined with line 74 to parts = info_dict['path'].split(os.sep)  ?
-        tmp = fs  # Naming conventions?
-        parts = relative_root.split(os.sep)
         # The folder indent / hierarchy counter
         counter = []
+        temp_dir_parts = dir_parts
+        relative_root = info_dict['path']  # Can this be combined with the next line ?
+        parts = relative_root.split(os.sep)  # parts = info_dict['path'].split(os.sep) ?
 
         # Checks if the directory exists in the list
         for part in parts:
             # Path has already been encountered
-            if part in tmp['paths']:
+            if part in temp_dir_parts['paths']:
                 # Append this level's count to our part's counter
-                counter.append(str(len(tmp['paths'])))
+                counter.append(str(len(temp_dir_parts['paths'])))
 
             # New path segment
             else:
-                counter.append(str(len(tmp['paths'])+1))
+                counter.append(str(len(temp_dir_parts['paths'])+1))
                 # Create this new part, with a value of 1.
                 if info_dict['type'] is 'folder':
-                    tmp['paths'][part] = {'paths': {}}
+                    temp_dir_parts['paths'][part] = {'paths': {}}
                 else:
-                    tmp['paths'][part] = {'paths': None}
+                    temp_dir_parts['paths'][part] = {'paths': None}
 
-            tmp = tmp['paths'][part]
+            temp_dir_parts = temp_dir_parts['paths'][part]
 
         # Checking if this is a file
-        if tmp['paths'] is None:
+        if temp_dir_parts['paths'] is None:
             img_type = '<span class="file">'
             file_type = '<td>File</td>'
-            f_size = '<td>{sizer}</td>'.format(sizer=size(info_dict['size'], system=alternative))
+            file_size = '<td>{sizer}</td>'.format(sizer=size(info_dict['size'], system=alternative))
         # This is a folder
         else:
             img_type = '<span class="folder">'
             file_type = '<td>Folder</td>'
-            f_size = '<td>{sizer}</td>'.format(sizer='--')
+            file_size = '<td>{sizer}</td>'.format(sizer='--')
 
         # If the folder depth is below the top level
         if len(counter) > 1:
-            par_count = counter[:]
-            del par_count[len(par_count)-1]
+            parent_count = counter[:]
+            del parent_count[len(parent_count)-1]
             # Creates a number value for the parent folder
-            par_id = '-'.join(par_count)
+            parent_id = '-'.join(parent_count)
             # Creates a number value for the current folder
             current_id = '-'.join(counter)
-            html += '<tr data-tt-id="{current_id}" data-tt-parent-id="{par_id}" data-path="{path}">' \
+            html += '<tr data-tt-id="{current_id}" data-tt-parent-id="{parent_id}" data-path="{path}">' \
                     '<td>{img_type}{name}' \
-                    '</span></td>{file_type}{size}</tr>\n'.format(current_id=current_id, par_id=par_id,
+                    '</span></td>{file_type}{size}</tr>\n'.format(current_id=current_id, parent_id=parent_id,
                                                                   path=relative_root, img_type=img_type,
-                                                                  name=part, file_type=file_type, size=f_size)
+                                                                  name=part, file_type=file_type, size=file_size)
         else:
             current_id = '-'.join(counter)
             html += '<tr data-tt-id="{current_id}" data-path="{path}"><td>{img_type}{name}</span>' \
                     '</td>{file_type}{size}' \
                     '</tr>\n'.format(current_id=current_id, path=relative_root, img_type=img_type,
-                                     name=part, file_type=file_type, size=f_size)
+                                     name=part, file_type=file_type, size=file_size)
     html += '</tbody>\n'
     return html
 
@@ -153,7 +177,7 @@ def index():
     # Initialize the unique ID counter for all files
     counter = 0
     # Walk the directory to collect file information. Returns "info".
-    walk(root)
+    walk(dir_root)
     #Set's unique IDs to each item of info
     for i in info:
         i['unique'] = counter
@@ -169,7 +193,7 @@ def dropzonify():
     # Clear old instances of info for a fresh data set
     del info[:]
     # Walk the directory to collect file information. Returns "info"
-    walk(root)
+    walk(dir_root)
     print json.dumps(info)  # A test line to verify that the output is correct / in the correct format.
     return render_template("dropzonify.html",
                            info=json.dumps(info))
@@ -188,7 +212,7 @@ def uploader():
             print "The file was posted"
             print requested_file  # A test line to verify that the output is correct / in the correct format.
             # Saves the file to the directory
-            requested_file.save(dir_top + os.sep + requested_file.filename)
+            requested_file.save(os.path.join(dir_root, requested_file.filename))
         # No file was passed to the page
         else:
             print "The requested file was not posted - no file sent in the request"
@@ -202,7 +226,7 @@ def uploader():
 def treetable():
     del info[:]
     # Walk the directory to collect file information. Returns "info"
-    walk(root)
+    walk(dir_root)
     print info  # A test line to verify that the output is correct / in the correct format.
     # Generates the HTML output for treetable
     html = table_gen(info)

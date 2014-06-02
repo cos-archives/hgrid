9bbb3ba02f79609702d80d63315bbd888dd1e715
sloria <sloria1@gmail.com>
2014-06-02 10:09:39 -0400

    Name the callbacks

--- plugins/hgrid-draggable/src/hgrid-draggable.js
+++ plugins/hgrid-draggable/src/hgrid-draggable.js
@@ -1,144 +1,158 @@
 /**
  * hgrid-draggable - Drag and drop support for HGrid
  */
+this.Draggable = (function($) {
+  'use strict';
 
-/**
- * Default options for the Slick.RowMoveManager constructor
- * @type {Object}
- */
-var rowMoveManagerDefaults = {
-  cancelEditOnDrag: true
-};
+  /**
+   * Default options for the Slick.RowMoveManager constructor
+   * @type {Object}
+   */
+  var rowMoveManagerDefaults = {
+    cancelEditOnDrag: true
+  };
 
-/**
- * Default options for Draggable constructor
- * @type {Object}
- */
-var defaults = {
+  /**
+   * Default options for Draggable constructor
+   * @type {Object}
+   */
+  var defaults = {
 
-  draggableNameCol: true,
-  // Additional options passed to the Slick.RowMoveManager constructor
-  rowMoveManagerOptions: {}
-};
-
-
-/** Public interface **/
-
-
-/**
- * Constructor for the HGrid.Draggable plugin.
- *
- * NOTE: This should NOT invoke the `init` method because `init` will be invoked
- * when HGrid#registerPlugin is called.
- */
-function Draggable(options) {
-  var self = this;
-  self.options = $.extend({}, defaults, options);
-  self.rowMoveManager = null;  // Initialized in init
-}
-
-// Initialization function called by HGrid#registerPlugin
-Draggable.prototype.init = function(grid) {
-  var self = this;
-  var data = grid.getData();
-  var dataView = grid.getDataView();
-  var slickgrid = grid.grid;
-
-  // Set selection model
-  slickgrid.setSelectionModel(new Slick.RowSelectionModel());
+    draggableNameCol: true,
+    // Additional options passed to the Slick.RowMoveManager constructor
+    rowMoveManagerOptions: {}
+  };
 
 
-  // Configure the RowMoveManager
-  var rowMoveManagerOptions = $.extend(
-    {}, rowMoveManagerDefaults, self.options.rowMoveManagerOptions
-  );
-  self.rowMoveManager = new Slick.RowMoveManager(rowMoveManagerOptions);
-  self.rowMoveManager.onBeforeMoveRows.subscribe(function(event, data) {
-    // Prevent moving row before or after itself
-    for (var i = 0; i < data.rows.length; i++) {
-      if (data.rows[i] === data.insertBefore || data.rows[i] === data.insertBefore - 1) {
-        event.stopPropagation;
-        return false;
+
+  /** Public interface **/
+
+  /**
+   * Constructor for the HGrid.Draggable plugin.
+   *
+   * NOTE: This should NOT invoke the `init` method because `init` will be invoked
+   * when HGrid#registerPlugin is called.
+   */
+  function Draggable(options) {
+    var self = this;
+    self.options = $.extend({}, defaults, options);
+    self.rowMoveManager = null;  // Initialized in init
+  }
+
+  // Initialization function called by HGrid#registerPlugin
+  Draggable.prototype.init = function(grid) {
+    var self = this;
+    var data = grid.getData();
+    var dataView = grid.getDataView();
+    var slickgrid = grid.grid;
+
+    // Set selection model
+    slickgrid.setSelectionModel(new Slick.RowSelectionModel());
+
+
+    // Configure the RowMoveManager
+    var rowMoveManagerOptions = $.extend(
+      {}, rowMoveManagerDefaults, self.options.rowMoveManagerOptions
+    );
+    self.rowMoveManager = new Slick.RowMoveManager(rowMoveManagerOptions);
+
+    /** Callbacks **/
+
+    var onBeforeMoveRows = function(event, data) {
+      // Prevent moving row before or after itself
+      for (var i = 0; i < data.rows.length; i++) {
+        if (data.rows[i] === data.insertBefore || data.rows[i] === data.insertBefore - 1) {
+          event.stopPropagation;
+          return false;
+        }
       }
-    }
-  });
-  self.rowMoveManager.onMoveRows.subscribe(function (event, args) {
-    var extractedRows = [];
-    var rows = args.rows;
-    var insertBefore = args.insertBefore;
-    var left = data.slice(0, insertBefore);
-    var right = data.slice(insertBefore, data.length);
+    };
 
-    rows.sort(function(a, b) { return a - b; });
+    // TODO(sloria): Test me
+    var onMoveRows = function (event, args) {
+      var extractedRows = [];
+      var rows = args.rows;
+      var insertBefore = args.insertBefore;
+      var left = data.slice(0, insertBefore);
+      var right = data.slice(insertBefore, data.length);
 
-    var i;
-    for (i = 0; i < rows.length; i++) {
-      extractedRows.push(data[rows[i]]);
-    }
+      rows.sort(function(a, b) { return a - b; });
 
-    rows.reverse();
-
-    for (i = 0; i < rows.length; i++) {
-      var row = rows[i];
-      if (row < insertBefore) {
-        left.splice(row, 1);
-      } else {
-        right.splice(row - insertBefore, 1);
+      var i;
+      for (i = 0; i < rows.length; i++) {
+        extractedRows.push(data[rows[i]]);
       }
-    }
+
+      rows.reverse();
+
+      for (i = 0; i < rows.length; i++) {
+        var row = rows[i];
+        if (row < insertBefore) {
+          left.splice(row, 1);
+        } else {
+          right.splice(row - insertBefore, 1);
+        }
+      }
 
 
-    data = left.concat(extractedRows.concat(right));
+      data = left.concat(extractedRows.concat(right));
 
-    var selectedRows = [];
-    for (i = 0; i < rows.length; i++) {
-      selectedRows.push(left.length + i);
-    }
+      var selectedRows = [];
+      for (i = 0; i < rows.length; i++) {
+        selectedRows.push(left.length + i);
+      }
 
 
-    slickgrid.resetActiveCell();
-    dataView.setItems(data);
-    slickgrid.setSelectedRows(selectedRows);
-    slickgrid.render();
+      slickgrid.resetActiveCell();
+      dataView.setItems(data);
+      slickgrid.setSelectedRows(selectedRows);
+      slickgrid.render();
 
-  });
+    };
 
-  // Register the slickgrid plugin
-  slickgrid.registerPlugin(self.rowMoveManager);
+    var onDragStart = function(event, dd) {
+      var cell = slickgrid.getCellFromEvent(e);
+      if (!cell) {
+        return;
+      }
 
-  slickgrid.onDragInit.subscribe(function(event) {
-    // prevent grid from cancelling drag'n'drop by default
-    event.stopImmediatePropagation;
-  });
+      dd.row = cell.row;
+      if (!data[dd.row]) {
+        return;
+      }
 
-  slickgrid.onDragStart.subscribe(function(event, dd) {
-    var cell = slickgrid.getCellFromEvent(e);
-    if (!cell) {
-      return;
-    }
+      if (Slick.GlobalEditorLock.isActive()) {
+        return;
+      }
 
-    dd.row = cell.row;
-    if (!data[dd.row]) {
-      return;
-    }
+      event.stopImmediatePropagation();
 
-    if (Slick.GlobalEditorLock.isActive()) {
-      return;
-    }
+      var selectedRows = grid.getSelectedRows();
 
-    event.stopImmediatePropagation();
+      if (!selectedRows.length || $.inArray(dd.row, selectedRows) === -1) {
+        selectedRows = [dd.row];
+        grid.setSelectedRows(selectedRows);
+      }
 
-    var selectedRows = grid.getSelectedRows();
+      dd.rows = selectedRows;
+      dd.count = selectedRows.length;
+    };
 
-    if (!selectedRows.length || $.inArray(dd.row, selectedRows) === -1) {
-      selectedRows = [dd.row];
-      grid.setSelectedRows(selectedRows);
-    }
+    self.rowMoveManager.onBeforeMoveRows.subscribe(onBeforeMoveRows);
+    self.rowMoveManager.onMoveRows.subscribe(onMoveRows);
 
-    dd.rows = selectedRows;
-    dd.count = selectedRows.length;
-  });
+    // Register the slickgrid plugin
+    slickgrid.registerPlugin(self.rowMoveManager);
 
-};
+    slickgrid.onDragInit.subscribe(function(event) {
+      // prevent grid from cancelling drag'n'drop by default
+      event.stopImmediatePropagation;
+    });
 
-HGrid.Draggable = Draggable;
+    slickgrid.onDragStart.subscribe(onDragStart);
+
+  };
+
+  HGrid.Draggable = Draggable;
+  return Draggable;
+}).call(this, jQuery);

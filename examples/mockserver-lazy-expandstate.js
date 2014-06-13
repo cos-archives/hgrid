ba0488caabe60c63ed1f9bd926575aa19750d022
sloria <sloria1@gmail.com>
2014-06-13 11:27:40 -0400

    Add getExpandState callback which allows saving expand/collapse state
    [resolves #78]

--- examples/mockserver-lazy-expandstate.js
+++ examples/mockserver-lazy-expandstate.js
@@ -1,10 +1,10 @@
 var endpoints = {
   '/grid/': [{name: 'Codes', slug: 'codes', kind: 'folder', children: []},
-              {name: 'Music', slug: 'music', kind: 'folder', children: []},
+              {name: 'Music', slug: 'music', kind: 'folder', expanded: true, children: []},
               {name: "Don't open me", slug: 'error', kind: 'folder', children: []}],
   '/grid/codes': [{name: 'foo.py', kind: 'item'}, {name: 'bar.js', kind: 'item'}],
   '/grid/music': [{name: 'psycho-killer.mp3', kind: 'item'},
-                  {name: 'Stones', kind: 'folder', slug: 'music/stones'}],
+                  {name: 'Stones', expanded: true, kind: 'folder', slug: 'music/stones'}],
   '/grid/music/stones': [{name: 'brownsugar.mp3', kind: 'item'}]
 };
 

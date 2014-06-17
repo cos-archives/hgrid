0f0031f81f6efb793779ff00bf23e5dd2c48177d
sloria <sloria1@gmail.com>
2014-06-17 16:43:54 -0400

    Build and bump version

--- dist/hgrid.js
+++ dist/hgrid.js
@@ -1,5 +1,5 @@
 /*
- *  HGrid - v0.1.3-dev
+ *  HGrid - v0.2.0
  *  A Javascript-based hierarchical grid that can be used to manage and organize files and folders
  */
 (function (global, factory) {
